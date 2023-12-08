c4a5f28ea0d67a703895ced8d460e3dc612a1ae4
anjufehno <lizakrug@tlu.ee>
2023-12-08 17:10:18 +0200

    создала папку API, там создала файл api где создала запрос из базы данных фильмов, вывожу best movies на главную страницу,

---
+++ API/api.ts
@@ -0,0 +1,15 @@
+export const getbestMovies = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/movie/best?",
+      {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.TMDB_API_AUTH}`,
+        },
+      }
+    );
+    const data = await res.json();
+  
+    return data.results;
+  };
+  
