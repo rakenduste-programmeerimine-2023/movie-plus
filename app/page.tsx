bc26026d2b927baaa6df7dade90f4bc6a4c9bc3c
anjufehno <lizakrug@tlu.ee>
2023-12-08 16:27:52 +0200

    создала папку API, там создала файл api где создала запрос из базы данных фильмов, вывожу best movies на главную страницу,

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
 import Header from '@/components/Header'
 import { cookies } from 'next/headers'
+import { getbestMovies } from "@/API/api";
 
 export default async function Index() {
   const cookieStore = cookies()
@@ -17,6 +18,7 @@ export default async function Index() {
   }
 
   const isSupabaseConnected = canInitSupabaseClient()
+  const bestMovies = await getbestMovies();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -30,6 +32,11 @@ export default async function Index() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
       </div>
+      <div className="потом буду делать css">
+        <h1 className="best">Best Movies</h1>z
+        <div>movies={bestMovies}</div> 
+      </div>
+
     </div>
   )
 }
