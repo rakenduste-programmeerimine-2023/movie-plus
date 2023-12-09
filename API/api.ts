df0aab434e057a87d05e83c6db589516bef9471b
anjufehno <lizakrug@tlu.ee>
2023-12-10 01:31:18 +0200

    добавила возможность делать запрос на фильмы комейдийного жанра которые отображаются на главной странице

--- API/api.ts
+++ API/api.ts
@@ -13,6 +13,21 @@ export const getbestMovies = async () => {
 
   return data.results;
 };
+export const getcomedy= async () => {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?&with_genres=35&without_genres=16&sort_by=popularity.desc ",
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_AUTH}`,
+      },
+    }
+  );
+  const data = await res.json();
+  console.log('data', data);
+
+  return data.results;
+}
 
 export const getdramaMovies = async () => {
   const res = await fetch(
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DeployButton from '../components/DeployButton'
 import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
-import { getbestMovies, getCartoons, getdramaMovies, getHorror, } from "@/API/api";
+import { getbestMovies, getcomedy, getdramaMovies, getHorror, getCartoons } from "@/API/api";
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Image from 'next/image'
@@ -21,9 +21,10 @@ export default async function Index() {
 
   const isSupabaseConnected = canInitSupabaseClient()
   const bestMovies = await getbestMovies();
-  const cartoons = await getCartoons();
+  const comedy = await getcomedy();
   const dramaMovies = await getdramaMovies();
   const horror = await getHorror();
+  const cartoons = await getCartoons();
   
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center bg-black">
@@ -38,6 +39,21 @@ export default async function Index() {
         <h1 className="best">Best movies</h1>
         <div className='flex flex-wrap w-full'>
           {bestMovies?.slice(0, 10)?.map((movie:any) => 
+          <div>
+            <Image 
+            width={300}
+            height={300}
+            src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} 
+            alt={movie?.title} />
+            <div>{movie?.title}</div>
+          </div>
+          )}
+          </div>
+      </div>
+      <div className="h-[600px] overflow-auto w-full">
+        <h1 className="best">Comedy movies</h1>
+        <div className='flex flex-wrap w-full'>
+          {comedy?.slice(0, 10)?.map((movie:any) => 
           <div>
             <Image 
             width={300}
