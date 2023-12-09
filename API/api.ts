e3f18536c190e83b506829f4a79c0a68b16316f4
anjufehno <lizakrug@tlu.ee>
2023-12-10 01:23:29 +0200

    добавила возможность делать запрос на фильмы жанра драма и выводить их на экран

--- API/api.ts
+++ API/api.ts
@@ -1,6 +1,6 @@
 export const getbestMovies = async () => {
   const res = await fetch(
-    "https://api.themoviedb.org/3/movie/popular",
+    "https://api.themoviedb.org/3/movie/popular?&with_genres=16&sort_by=popularity.desc",
     {
       headers: {
         accept: "application/json",
@@ -13,6 +13,22 @@ export const getbestMovies = async () => {
 
   return data.results;
 };
+
+export const getdramaMovies = async () => {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?&with_genres=18&sort_by=popularity.desc",
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_AUTH}`,
+      },
+    }
+  );
+  const data = await res.json();
+  console.log('data', data);
+
+  return data.results;
+}
 
 export const getCartoons = async () => {
   const res = await fetch(
