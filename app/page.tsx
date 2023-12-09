1ada12a4381ee9603b1703ffe6241ed298db51d6
anjufehno <lizakrug@tlu.ee>
2023-12-10 00:41:48 +0200

    добавила возможность делать запрос на хоррор фильмы и выводить их на экран

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DeployButton from '../components/DeployButton'
 import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
-import { getCartoons, getbestMovies } from "@/API/api";
+import { getCartoons, getbestMovies, getHorror, } from "@/API/api";
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Image from 'next/image'
@@ -22,6 +22,8 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient()
   const bestMovies = await getbestMovies();
   const cartoons = await getCartoons();
+  const horror = await getHorror();
+  
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center bg-black">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
@@ -35,6 +37,21 @@ export default async function Index() {
         <h1 className="best">Best Movies</h1>
         <div className='flex flex-wrap w-full'>
           {bestMovies?.slice(0, 10)?.map((movie:any) => 
+          <div>
+            <Image 
+            width={300}
+            height={300}
+            src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} 
+            alt={movie?.title} />
+            <div>{movie?.title}</div>
+          </div>
+          )}
+          </div>
+      </div>
+      <div className="h-[600px] overflow-auto w-full">
+        <h1 className="best">Horror Movies</h1>
+        <div className='flex flex-wrap w-full'>
+          {horror?.slice(0, 10)?.map((movie:any) => 
           <div>
             <Image 
             width={300}
