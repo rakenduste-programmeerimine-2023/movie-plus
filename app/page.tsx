718c88503c240cc500490d5036a17adcf70d5008
anjufehno <lizakrug@tlu.ee>
2023-12-09 22:01:11 +0200

    добавила возможность делать запрос на раздел мультики  и вывела мультики на главный экран

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DeployButton from '../components/DeployButton'
 import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
-import { getbestMovies } from "@/API/api";
+import { getCartoons, getbestMovies } from "@/API/api";
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Image from 'next/image'
@@ -21,7 +21,7 @@ export default async function Index() {
 
   const isSupabaseConnected = canInitSupabaseClient()
   const bestMovies = await getbestMovies();
-  console.log('bestMovies', bestMovies)
+  const cartoons = await getCartoons();
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center bg-black">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
@@ -31,10 +31,25 @@ export default async function Index() {
           <Header/>
         </div>
       </nav>
-      <div className="потом буду делать css">
+      <div className="h-[600px] overflow-auto w-full">
         <h1 className="best">Best Movies</h1>
-        <div>
-          {bestMovies?.map(movie => 
+        <div className='flex flex-wrap w-full'>
+          {bestMovies?.slice(0, 10)?.map((movie:any) => 
+          <div>
+            <Image 
+            width={300}
+            height={300}
+            src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} 
+            alt={movie?.title} />
+            <div>{movie?.title}</div>
+          </div>
+          )}
+          </div>
+      </div>
+      <div className="h-[600px] overflow-auto w-full">
+        <h1 className="best">Cartoons</h1>
+        <div className='flex flex-wrap w-full'>
+          {cartoons?.slice(0, 10).map((movie:any) => 
           <div>
             <Image 
             width={300}
