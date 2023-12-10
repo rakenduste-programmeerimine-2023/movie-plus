8e22461e066ac7c60a6e1fc7e6f6e344b64c1e09
anjufehno <lizakrug@tlu.ee>
2023-12-10 02:48:29 +0200

    сама создала logo для footer чтобы в дальнейшем это была копка для feetback, после подключила картинку к footer и немного поработала с дизайном

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,26 @@
+import Image from 'next/image';
+import logof from '@/components/Logo/logof.jpg';
+
 function Footer() {
   return (
-      <div className="flex-1 w-full flex flex-col gap-30 items-left place-content-end">
-        <h1>© {new Date().getFullYear()} All rights reserved</h1>
+    <div className="flex-1 w-full flex flex-col gap-30 items-left place-content-end">
+      <h1>© {new Date().getFullYear()} All rights reserved</h1>
+      <div className="logof-container" style={{ position: 'relative' }}>
+        <Image
+          src={logof}
+          alt="Logof"
+          className="logof"
+          style={{
+            position: 'absolute',
+            bottom: '10px',
+            right: '10px',
+            width: '300px', // Задайте требуемую ширину
+            height: 'auto', // Установите высоту "auto", чтобы пропорции сохранялись
+          }}
+        />
+      </div>
     </div>
   );
 }
 
 export default Footer;
-
