7d6db1f3d8c36c526543cf5bd6cb4a9544437851
anjufehno <lizakrug@tlu.ee>
2023-12-11 00:57:45 +0200

    сделала issue #33

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,38 @@
 import Image from 'next/image';
 import logof from '@/components/image/logof.jpg';
-
+import newslogo from '@/components/image/newslogo.jpg';
 function Footer() {
-  return (
-    <div className="flex-1 w-full flex flex-col gap-30 items-left place-content-end">
-      <h1>© {new Date().getFullYear()} All rights reserved</h1>
-      <div className="logof-container" style={{ position: 'relative' }}>
-        <Image
-          src={logof}
-          alt="Logof"
-          className="logof"
-          style={{
-            position: 'absolute',
-            bottom: '10px',
-            right: '10px',
-            width: '300px',
-            height: 'auto',
-          }}
-        />
+    return (
+      <div className="flex-1 w-full flex flex-col gap-30 items-left place-content-end">
+        <h1>© {new Date().getFullYear()} All rights reserved</h1>
+        <div className="logof-newslogo-container" style={{ position: 'relative', display: 'flex', justifyContent: 'flex-end' }}>
+          <div style={{ position: 'relative', marginRight: '20px' }}>
+            <Image
+              src={newslogo}
+              alt="newslogo"
+              className="newslogo"
+              style={{
+                width: '300px',
+                height: 'auto',
+              }}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <Image
+              src={logof}
+              alt="Logof"
+              className="logof"
+              style={{
+                width: '300px',
+                height: 'auto',
+              }}
+            />
+          </div>
+        </div>
       </div>
-    </div>
-  );
-}
-
-export default Footer;
+    );
+  }
+  
+  export default Footer;
+  
+  
