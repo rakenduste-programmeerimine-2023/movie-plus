3a5de22661cbec30dc018ae7daaea28cfa4ba34b
anjufehno <lizakrug@tlu.ee>
2023-12-11 20:17:26 +0200

    test

--- components/AboutUsButton.tsx
+++ components/AboutUsButton.tsx
@@ -2,7 +2,7 @@ export default function AboutUsButton() {
     return (
       <a
         className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[5cm]">
-        About us
+        About us (тест момент)
       </a>
     )
   }
