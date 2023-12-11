5b4333217084405f1c4a5d6ed969e5a15ed746e8
anjufehno <lizakrug@tlu.ee>
2023-12-11 18:52:00 +0200

    сделала #40 about us button

---
+++ components/AboutUsButton.tsx
@@ -0,0 +1,8 @@
+export default function AboutUsButton() {
+    return (
+      <a
+        className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute right-0 left-0 m-4">
+        About us
+      </a>
+    )
+  }
