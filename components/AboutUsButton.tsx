788826e0299ad97b8ea8da8171009ea98c8ac80a
anjufehno <lizakrug@tlu.ee>
2023-12-11 20:14:15 +0200

    test

--- components/AboutUsButton.tsx
+++ components/AboutUsButton.tsx
@@ -1,7 +1,7 @@
 export default function AboutUsButton() {
     return (
       <a
-        className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute right-0 left-0 m-4">
+        className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[5cm]">
         About us
       </a>
     )
--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -33,7 +33,7 @@ export default async function AuthButton() {
   ) : (
     <Link
       href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-black hover:bg-btn-background-hover text-white">
+      className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-4">
       Login
   </Link>
   )
