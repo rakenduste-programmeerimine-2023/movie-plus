cce3336e9635edc2b28696220e7559dcc01610f2
anjufehno <lizakrug@tlu.ee>
2023-12-12 16:15:45 +0200

    sign up button #7

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import SignUpButton from '../components/SignUpButton'
 import AboutUsButton from '../components/AboutUsButton'
 import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
@@ -29,6 +30,7 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <AboutUsButton />
+          <SignUpButton/>
           {isSupabaseConnected && <AuthButton />}
           <Header/>
         </div>
