706e2aceb0d7ac4ab875d9643aa3b5f807cc8f06
anjufehno <lizakrug@tlu.ee>
2023-12-12 16:57:14 +0200

    sign up button #7

---
+++ aboutUs/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <a
+          className="py-4 px-6 rounded-md no-underline hover:bg-btn-background-hover border font-thin text-xl">
+          About us
+        </a>
+      </div>
+    );
+  }
+  
--- components/AboutUsButton.tsx
+++ components/AboutUsButton.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 export default function AboutUsButton() {
     return (
-      <a
-        className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[5cm]">
-        About us
-      </a>
+      <div>
+        <Link href="/aboutUs">
+          <button className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[5cm] font-thin">About us</button>
+        </Link>
+      </div>
     )
   }
