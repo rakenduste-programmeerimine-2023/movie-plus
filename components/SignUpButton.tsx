8ec33ce2633241d1a12ffd98651cba0e55244f29
anjufehno <lizakrug@tlu.ee>
2023-12-12 16:15:45 +0200

    sign up button #7

---
+++ components/SignUpButton.tsx
@@ -0,0 +1,8 @@
+export default function SignUpButton() {
+    return (
+      <a
+        className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[2.5cm]">
+        Sign up
+      </a>
+    )
+  }
