4473faacaeb3053d1f31b2e289120d49f860c9c3
anjufehno <lizakrug@tlu.ee>
2023-12-12 20:33:55 +0200

    about us page #39

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
 import minuLogo from '@/image/ogg.jpg';
+import Link from 'next/dist/client/link';
 
 const Header: React.FC = () => {
   return (
-    <div className="logo">
-      <Image src={minuLogo} alt="Logo" width={300} height={300} className="absolute top-0 right-0"/>
-    </div>
+    <Link href="http://localhost:3001/">
+      <div className="logo">
+      <Image src={minuLogo} alt="Logo" width={300} height={300} className="absolute top-0 right-0"/>  
+      </div>
+    </Link>
   );
 };
 export default Header;
+
