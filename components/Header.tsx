1d01aec3a0db9c46a0ee61f31fa316081bc4e02d
anjufehno <lizakrug@tlu.ee>
2023-12-20 18:46:23 +0200

    refactor: rewrite header, add site logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import Image from 'next/image';
+import ogg from '@/image/ogg.jpg';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ marginRight: '200px' }}>
+      </div>
+      <Link href="http://localhost:3000/">
+        <div className="logo cursor-pointer">
+          <Image src={ogg} alt="Logo" width={300} height={300} />
+        </div>
+      </Link>
+    </div>)
+}
 
 export default Header;
-import HeaderComponent from './HeaderComponent';
-
-function Header() {
-  return <HeaderComponent />;
-}
