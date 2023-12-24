fb8b1603fef9950d886a3494e4c528ef37c2681f
Danistol <raigosei@tlu.ee>
2023-12-24 18:03:06 +0200

    feat(new page): added Newsletter page with a form to fill

---
+++ app/newsletter/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export default function NewsletterPage() {
+  const [formData, setFormData] = useState<FormData>({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubscribe = () => {
+    console.log("Subscribed:", formData);
+  };
+
+  return (
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+      <Link
+        href="/"
+        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+        >
+          <polyline points="15 18 9 12 15 6" />
+        </svg>{" "}
+        Back
+      </Link>
+
+      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+        <label className="text-md" htmlFor="firstName">
+          First Name
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="text"
+          name="firstName"
+          placeholder="Nimi"
+          value={formData.firstName}
+          onChange={handleChange}
+          required
+        />
+
+        <label className="text-md" htmlFor="lastName">
+          Last Name
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="text"
+          name="lastName"
+          placeholder="Perekonnanimi"
+          value={formData.lastName}
+          onChange={handleChange}
+          required
+        />
+
+        <label className="text-md" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="email"
+          name="email"
+          placeholder="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <button
+          type="button"
+          onClick={handleSubscribe}
+          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+        >
+          Subscribe
+        </button>
+      </form>
+    </div>
+  );
+}
