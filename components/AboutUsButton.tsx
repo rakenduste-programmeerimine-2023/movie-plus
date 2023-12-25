85638f97d69fcd85e1793dd05c4335a59ec5be51
anjufehno <lizakrug@tlu.ee>
2023-12-25 17:36:21 +0200

    feat: add about us

--- components/AboutUsButton.tsx
+++ components/AboutUsButton.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function AboutUsButton() {
     return (
       <div>
+        <Link href="/aboutUs">
           <button className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[5cm] font-thin">About us</button>
+        </Link>
       </div>
     )
-  }
+}
