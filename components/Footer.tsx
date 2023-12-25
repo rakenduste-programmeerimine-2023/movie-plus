eaedc5ddabc32a0d072f8b43bceb34ced553f7d4
anjufehno <lizakrug@tlu.ee>
2023-12-25 11:59:39 +0200

    refactor: rewrite footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import Image from 'next/image';
+import logof from '@/image/logof.jpg';
+import newslogo from '@/image/newslogo.jpg';
 
 function Footer() {
   return (
-    <h1>&copy; {new Date().getFullYear()} All rights reserved</h1>
+    <div className="flex-1 w-full flex flex-col gap-30 items-center place-content-end">
+      <div>
+        <div>
+          <Image
+            src={logof}
+            alt="Logof"
+            className="logof"
+            width={200}
+            height={100}
+          />
+        </div>
+        <div>
+          <Image
+            src={newslogo}
+            alt="newslogo"
+            className="newslogo"
+            width={200}
+            height={100}
+          />
+        </div>
+      </div>
+      <h1>&copy; {new Date().getFullYear()} All rights reserved</h1>
+    </div>
   );
 }
 
 export default Footer;
+
