cbe7f4870d4689426554ee9dbaad15ecea6ef9bb
anjufehno <lizakrug@tlu.ee>
2023-12-30 15:56:45 +0200

    style: rewrite about us button style

--- components/AboutUsButton.tsx
+++ components/AboutUsButton.tsx
@@ -5,7 +5,7 @@ export default function AboutUsButton() {
     return (
       <div>
         <Link href="/aboutUs">
-          <button className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[5cm] font-thin">About us</button>
+          <button className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[2.5cm] font-thin">About us</button>
         </Link>
       </div>
     )
