c2e9572fa5dfe9864df6098169376651bf80c072
anjufehno <lizakrug@tlu.ee>
2024-01-01 14:53:00 +0200

    feat: add TMDB API auth

---
+++ app/myPage/page.tsx
@@ -0,0 +1,30 @@
+import AboutUsButton from '../../components/AboutUsButton';
+import AuthButton from '../../components/AuthButton';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Image from 'next/image';
+import login from '@/image/login.jpg';
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-black text-white">
+      <nav className="w-full border-b border-b-foreground/10 h-16">
+        <div className="max-w-4xl flex justify-between items-center p-3 text-sm mx-auto">
+          <AboutUsButton />
+          <AuthButton />
+        </div>
+      </nav>
+      <div className="button-container relative flex flex-col items-center mt-5">
+        <div className="flex items-end">
+          <div className="mr-4">
+            <Image src={login} alt="Logo" width={250} height={250} />
+          </div>
+        </div>
+      </div>
+      <Footer />
+      <div className="header-component absolute top-0 right-0">
+        <Header />
+      </div>
+    </div>
+  );
+}
---
+++ components/myPageButton.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function myPageButton() {
+    return (
+      <div>
+        <Link href="/myPage">
+          <button className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border absolute top-0 left-0 m-4 ml-[2.5cm] font-thin">About us</button>
+        </Link>
+      </div>
+    )
+}
