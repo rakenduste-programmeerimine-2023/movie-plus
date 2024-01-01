faefdacbf2140e78aa8b51af8cdcaab40e5dde11
anjufehno <lizakrug@tlu.ee>
2024-01-01 15:14:43 +0200

    refactor: rewrite api.ts file, add horror movies API

--- API/api.ts
+++ API/api.ts
@@ -44,3 +44,19 @@ export const getdramaMovies = async () => {
   
     return data.results;
 };
+
+export const gethorror = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?&with_genres=27",
+      {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.TMDB_API_AUTH}`,
+        },
+      }
+    );
+    const data = await res.json();
+    console.log('data', data);
+  
+    return data.results;
+};
