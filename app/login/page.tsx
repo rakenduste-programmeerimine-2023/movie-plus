59fc19dfd7bd3be8c1c7f49c2d65af16b16ce37f
Evelinotsa <135664496+Evelinotsa@users.noreply.github.com>
2024-01-01 23:30:34 +0200

    feat: add sign up page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,30 +28,6 @@ export default function Login({
     return redirect('/')
   }
 
-  const signUp = async (formData: FormData) => {
-    'use server'
-
-    const origin = headers().get('origin')
-    const email = formData.get('email') as string
-    const password = formData.get('password') as string
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    })
-
-    if (error) {
-      return redirect('/login?message=Could not authenticate user')
-    }
-
-    return redirect('/login?message=Check email to continue sign in process')
-  }
-
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
@@ -100,12 +76,6 @@ export default function Login({
         />
         <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
           Sign In
-        </button>
-        <button
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-        >
-          Sign Up
         </button>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
