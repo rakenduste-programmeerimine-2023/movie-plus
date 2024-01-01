215de7e07f12c36170bf8124ba99ed9764db851d
anjufehno <lizakrug@tlu.ee>
2024-01-01 15:17:24 +0200

    refactor: rewrite api.ts file, addcartoons API

--- API/api.ts
+++ API/api.ts
@@ -60,3 +60,19 @@ export const gethorror = async () => {
   
     return data.results;
 };
+
+export const getCartoons = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?&with_genres=16",
+      {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.TMDB_API_AUTH}`,
+        },
+      }
+    );
+    const data = await res.json();
+    console.log('data', data);
+  
+    return data.results;
+};
