4d8ac9c465f878a147ee6983373a102b4287c57a
anjufehno <lizakrug@tlu.ee>
2024-01-02 13:23:47 +0200

    refactor: rewrite movie card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,21 +1,23 @@
 "use client";
 import Image from "next/image";
-
+import Link from "next/link";
 import React from "react";
 
 export default function MovieCard({
   poster_path,
   title,
+  id
 }: {
   poster_path: string;
   title: string;
+  id: string;
 }) {
   return (
-    <li>
+    <Link href="/description/${id}">
       <Image
         src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${poster_path}`}
         alt={title}
       />
-    </li>
+    </Link>
   );
 }
