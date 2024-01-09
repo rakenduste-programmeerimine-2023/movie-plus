eca382de7ff55004d05a680456e7331f64fa8db4
anjufehno <lizakrug@tlu.ee>
2024-01-09 12:47:53 +0200

    refactor: rename about us page to about me

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -1,14 +1,17 @@
-import AboutUsButton from '../../components/AboutUsButton';
+import AboutUsButton from '../../components/AboutMeButton';
 import AuthButton from '../../components/AuthButton';
 import Header from '@/components/Header';
 import Image from 'next/image';
 import me from '@/image/me.jpg';
-import notme from '@/image/notme.jpg';
 import Footer from '@/components/Footer';
 
 export default function Page() {
   return (
+    
     <div className="flex flex-col items-center min-h-screen bg-black text-white">
+      <div className="header-component absolute top-0 right-0">
+        <Header />
+      </div>
       <nav className="w-full border-b border-b-foreground/10 h-16">
         <div className="max-w-4xl flex justify-between items-center p-3 text-sm mx-auto">
           <AboutUsButton />
@@ -16,23 +19,16 @@ export default function Page() {
         </div>
       </nav>
       <div className="button-container relative flex flex-col items-center mt-5">
-      <a className="text-white font-thin text-4xl mb-2" style={{ marginTop: '10px' }}>About us</a>
+      <a className="text-white font-thin text-4xl mb-2" style={{ marginTop: '10px' }}>About me</a>
         <div className="flex items-end">
           <div className="mr-4">
             <Image src={me} alt="Logo" width={250} height={250} />
             <p className="font-thin text-left">telegram:@kakskolmneli</p>
           </div>
-          <div className="ml-4">
-            <Image src={notme} alt="Logo" width={250} height={250} />
-            <p className="font-thin text-left">telegram:@Puljakai</p>
-          </div>
         </div>
       </div>
       <div className="footer-component">
         <Footer/>
-      </div>
-      <div className="header-component absolute top-0 right-0">
-        <Header />
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import AboutUsButton from "../components/AboutUsButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
@@ -12,6 +11,7 @@ import {
 import MovieCard from "@/components/MovieCard";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import AboutMeButton from "../components/AboutMeButton";
 
 
 export default async function Index() {
@@ -36,7 +36,7 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-10 items-center bg-black">
       <nav className="w-full flex justify-center border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <AboutUsButton />
+          <AboutMeButton />
           {isSupabaseConnected && <AuthButton />}
         </div>
         <div className="absolute top-0 right-0 h-10 style={{ height: '8px' }}">
