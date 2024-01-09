88788131cba19cc37d5f7d8571c33dee68b2b178
anjufehno <lizakrug@tlu.ee>
2024-01-09 12:23:55 +0200

    feat: add search component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import ogg from '@/image/ogg.jpg';
 import Link from 'next/link';
+import Search from '@/components/Search';
 
 const Header: React.FC = () => {
   return (
-    <div style={{ display: 'flex', alignItems: 'center'}}>
+    <div style={{ display: 'flex', alignItems: 'center',  color: 'black'}}>
       <div style={{ marginRight: '200px' }}>
+        <Search />
       </div>
       <Link href="http://localhost:3000/">
         <div className="logo cursor-pointer">
