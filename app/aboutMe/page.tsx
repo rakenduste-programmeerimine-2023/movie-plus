1c866999e1b10b6b599d7592e1b8cc5b9ca147c1
anjufehno <lizakrug@tlu.ee>
2024-01-10 16:58:58 +0200

    style: add style to about me page

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -3,6 +3,8 @@ import AuthButton from '../../components/AuthButton';
 import Header from '@/components/Header';
 import Image from 'next/image';
 import me from '@/image/me.jpg';
+import aboutMeStyle from '@/image/aboutMeStyle.jpg';
+import aboutMeSecondStyle from '@/image/aboutMeSecondStyle.jpg';
 import Footer from '@/components/Footer';
 
 export default function Page() {
@@ -18,14 +20,22 @@ export default function Page() {
           <AuthButton />
         </div>
       </nav>
-      <div className="button-container relative flex flex-col items-center mt-5">
+      <div className="button-container relative flex flex-col items-center mt-5 font-thin">
       <a className="text-white font-thin text-4xl mb-2" style={{ marginTop: '10px' }}>About me</a>
         <div className="flex items-end">
-          <div className="mr-4">
-            <Image src={me} alt="Logo" width={250} height={250} />
-            <p className="font-thin text-left">telegram:@kakskolmneli</p>
-          </div>
-        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div className="mr-4">
+          <Image src={aboutMeSecondStyle} alt="style" width={250} height={250} />
+      </div>
+       <div className="mr-4">
+        <Image src={me} alt="Logo" width={250} height={250} />
+          <p className="font-thin text-left">telegram:@kakskolmneli</p>
+      </div>
+      <div className="mr-4">
+        <Image src={aboutMeStyle} alt="style" width={250} height={250} />
+      </div>
+      </div>
+      </div>
       </div>
       <div className="footer-component">
         <Footer/>
