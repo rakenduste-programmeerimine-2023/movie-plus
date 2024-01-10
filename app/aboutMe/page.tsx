c685197a0aab36642d58704ba76dd3118a729ebc
anjufehno <lizakrug@tlu.ee>
2024-01-10 17:22:15 +0200

    style: add style to profil page, add style to about mepage

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -9,36 +9,36 @@ import Footer from '@/components/Footer';
 
 export default function Page() {
   return (
-    
     <div className="flex flex-col items-center min-h-screen bg-black text-white">
       <div className="header-component absolute top-0 right-0">
         <Header />
       </div>
-      <nav className="w-full  h-16">
+      <nav className="w-full h-16">
         <div className="max-w-4xl flex justify-between items-center p-3 text-sm mx-auto">
           <AboutUsButton />
           <AuthButton />
         </div>
       </nav>
       <div className="button-container relative flex flex-col items-center mt-5 font-thin">
-      <a className="text-white font-thin text-4xl mb-2" style={{ marginTop: '10px' }}>About me</a>
+        <a className="text-white font-thin text-4xl mb-2" style={{ marginTop: '10px' }}>About me</a>
         <div className="flex items-end">
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div className="mr-4">
-          <Image src={aboutMeSecondStyle} alt="style" width={250} height={250} />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="mr-4">
+              <Image src={aboutMeSecondStyle} alt="style" width={250} height={250} />
+            </div>
+            <div className="mr-4">
+              <Image src={me} alt="Logo" width={250} height={250} />
+              <p className="font-thin text-left">telegram:@kakskolmneli</p>
+              <p className="font-thin text-left">instagram:@anjufehno</p>
+            </div>
+            <div className="mr-4">
+              <Image src={aboutMeStyle} alt="style" width={250} height={250} />
+            </div>
+          </div>
+        </div>
       </div>
-       <div className="mr-4">
-        <Image src={me} alt="Logo" width={250} height={250} />
-          <p className="font-thin text-left">telegram:@kakskolmneli</p>
-      </div>
-      <div className="mr-4">
-        <Image src={aboutMeStyle} alt="style" width={250} height={250} />
-      </div>
-      </div>
-      </div>
-      </div>
-      <div className="footer-component">
-        <Footer/>
+      <div className="footer-component" style={{ marginTop: '-100px' }}>
+        <Footer />
       </div>
     </div>
   );
