000811f01929d52d5257aeab8601546d29481645
anjufehno <lizakrug@tlu.ee>
2024-01-11 21:43:50 +0200

    style: add style to about me page

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import me from '@/image/me.jpg';
 import aboutMeStyle from '@/image/aboutMeStyle.jpg';
 import aboutMeSecondStyle from '@/image/aboutMeSecondStyle.jpg';
+import butterfly from '@/image/butterfly.jpg';
 import Footer from '@/components/Footer';
 
 export default function Page() {
@@ -23,8 +24,12 @@ export default function Page() {
         <a className="text-white font-thin text-4xl mb-2" style={{ marginTop: '10px' }}>About me</a>
         <div className="flex items-end">
           <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div className="mr-4">
+          <div style={{ display: 'flex', flexDirection: 'row', position: 'relative' }}>
+              <div className="mr-4" style={{ position: 'relative', bottom: '100px' }}>
               <Image src={aboutMeSecondStyle} alt="style" width={250} height={250} />
+            </div>
+            <div style={{ position: 'absolute', bottom: -15, left: -280 }}>
+              <Image src={butterfly} alt="style" width={300} height={300} />
             </div>
             <div className="mr-4">
               <Image src={me} alt="Logo" width={250} height={250} />
@@ -34,6 +39,10 @@ export default function Page() {
             <div className="mr-4">
               <Image src={aboutMeStyle} alt="style" width={250} height={250} />
             </div>
+            <div style={{ position: 'absolute', top: -50, right: -200 }}>
+              <Image src={butterfly} alt="style" width={300} height={300} />
+            </div>
+          </div>
           </div>
         </div>
       </div>
