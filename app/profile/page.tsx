394e8cd8f96caa4571dd22e48cf237f7e1386be0
anjufehno <lizakrug@tlu.ee>
2024-01-12 11:44:22 +0200

    refactor: changed style in profil page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -13,9 +13,8 @@ import Header2 from '@/components/Header2';
 export default async function Page() {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+
+  const { data: { user } } = await supabase.auth.getUser();
 
   if (!user) return redirect('/');
 
@@ -30,25 +29,21 @@ export default async function Page() {
         <Header2 />
       </div>
       <div className="button-container relative mt-5 text-center">
-        <div className="flex flex-col items-center">
-          <div style={{ display: 'flex' }}>
-          <div className="flex justify-center">
-  <div className="mr-4">
-    <Image src={profilImage} alt="style" width={600} height={600} />
-  </div>
-  <div className="mb-4 mr-4 flex flex-col items-center">
-    <Image src={login} alt="Logo" width={300} height={300} />
-    <div className="text-white-500 text-sm font-thin bg-opacity-75 p-2 max-w-xs">
-      Hello🩷! This page is created for authorized users. I have two mini-surveys for you, which you can take by clicking on the cute animals in the bottom left corner of the screen. Have a great day 💗.
-    </div>
-  </div>
-  <div className="mr-4">
-    <Image src={profilSecondImage} alt="style" width={400} height={400} />
-  </div>
-  <div className="mr-4" style={{ position: 'relative', top: '300px' }}>
-    <Image src={profilThirdImage} alt="style" width={200} height={200} />
-  </div>
-</div>
+        <div className="flex justify-center">
+          <div className="mr-4">
+            <Image src={profilImage} alt="style" width={350} height={350} />
+          </div>
+          <div className="mb-4 mr-4 flex flex-col items-center">
+            <Image src={login} alt="Logo" width={300} height={300} />
+            <div className="text-white-500 text-sm font-thin bg-opacity-75 p-2 max-w-xs">
+              Hello🩷! This page is created for authorized users. I have two mini-surveys for you, which you can take by clicking on the cute animals in the bottom left corner of the screen. Have a great day 💗.
+            </div>
+          </div>
+          <div className="mr-4">
+            <Image src={profilSecondImage} alt="style" width={200} height={200} />
+          </div>
+          <div className="mr-4" style={{ position: 'relative', top: '300px' }}>
+            <Image src={profilThirdImage} alt="style" width={200} height={200} />
           </div>
         </div>
       </div>
